b7ea60984ab1d5246720e97f72ed694725cfa2ae
Adrian Bucur <adrian.bucur@yap.com>
2022-12-06 16:00:42 +0200

    first commit

---
+++ src/main/java/com/example/demo/HelloServlet.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "mesaj din servLet";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+    public void destroy() {
+    }
+}
